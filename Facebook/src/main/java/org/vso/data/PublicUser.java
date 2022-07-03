f574103a8492af33571a4e9581413af8c4db5022
tsenovi <vandertsen@gmail.com>
2022-07-04 01:35:22 +0300

    Created tests for Authentication.Login and updated PublicUser

--- Facebook/src/main/java/org/vso/data/PublicUser.java
+++ Facebook/src/main/java/org/vso/data/PublicUser.java
@@ -2,17 +2,22 @@ package org.vso.data;
 
 public class PublicUser {
 
+    private final long id;
     private final String email;
     private final String firstName;
     private final String lastName;
     private final int age;
 
-
     public PublicUser(User loggedUser) {
+        this.id = loggedUser.getId();
         this.email = loggedUser.getEmail();
         this.firstName = loggedUser.getFirstName();
         this.lastName = loggedUser.getLastName();
         this.age = loggedUser.getAge();
+    }
+
+    public long getId() {
+        return id;
     }
 
     public String getEmail() {
--- Facebook/src/test/java/org/vso/domain/AuthenticationServiceTest.java
+++ Facebook/src/test/java/org/vso/domain/AuthenticationServiceTest.java
@@ -1,7 +1,9 @@
 package org.vso.domain;
 
 import org.junit.jupiter.api.Test;
+import org.vso.constants.LoginStatus;
 import org.vso.constants.RegistrationStatus;
+import org.vso.dto.UserLoginDTO;
 import org.vso.dto.UserRegistrationDTO;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,5 +24,19 @@ class AuthenticationServiceTest {
                 "Georgi", "Ivanov", 21);
         var authenticationService = AuthenticationService.getInstance();
         assertEquals(RegistrationStatus.REGISTRATION_SUCCESSFUL, authenticationService.registerUser(userRegistrationDTO));
+    }
+
+    @Test
+    void testLoginWhenUserExistsThenLoginSuccessful() {
+        UserLoginDTO userLoginDTO = new UserLoginDTO("ivan@gmail.com", "111");
+        var authenticationService = AuthenticationService.getInstance();
+        assertEquals(LoginStatus.LOGIN_SUCCESSFUL, authenticationService.login(userLoginDTO));
+    }
+
+    @Test
+    void testLoginWhenUserNotExistsThenLoginFailed() {
+        UserLoginDTO userLoginDTO = new UserLoginDTO("ivan111@gmail.com", "909");
+        var authenticationService = AuthenticationService.getInstance();
+        assertEquals(LoginStatus.LOGIN_FAILED, authenticationService.login(userLoginDTO));
     }
 }
