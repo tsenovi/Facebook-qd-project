7619d33f5fe165ad64ae6f50d002a42637fe487a
tsenovi <vandertsen@gmail.com>
2022-07-04 23:43:20 +0300

    Updated PublicUser

--- Facebook/src/main/java/org/vso/data/PublicUser.java
+++ Facebook/src/main/java/org/vso/data/PublicUser.java
@@ -3,36 +3,12 @@ package org.vso.data;
 public class PublicUser {
 
     private final long id;
-    private final String email;
-    private final String firstName;
-    private final String lastName;
-    private final int age;
 
     public PublicUser(User loggedUser) {
         this.id = loggedUser.getId();
-        this.email = loggedUser.getEmail();
-        this.firstName = loggedUser.getFirstName();
-        this.lastName = loggedUser.getLastName();
-        this.age = loggedUser.getAge();
     }
 
     public long getId() {
         return id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public int getAge() {
-        return age;
     }
 }
