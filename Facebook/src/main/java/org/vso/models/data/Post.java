d19d2c622d0db8edcfd750e08e3c9108cf153427
KirilDimitrov86 <92857404+KirilDimitrov86@users.noreply.github.com>
2022-07-16 18:34:42 +0300

    fixed LocalDateTime variable

---
+++ Facebook/src/main/java/org/vso/models/data/Post.java
@@ -0,0 +1,53 @@
+package org.vso.models.data;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "posts")
+public class Post {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+        @Column(nullable = false, unique = false, length = 45)
+        private String content;
+        @Basic
+        private LocalDateTime dateTime;
+        @ManyToOne(optional = false,cascade = CascadeType.PERSIST)
+        @JoinColumn(name = "USER_ID",referencedColumnName = "ID")
+        private User author;
+
+        protected Post(){
+
+        }
+
+        public Post(String content, User author) {
+                this.dateTime = LocalDateTime.now();
+                this.content = content;
+                this.author = author;
+        }
+
+        public Long getId() {
+                return id;
+        }
+
+        public String getContent() {
+                return content;
+        }
+
+        public void setContent(String content) {
+                this.content = content;
+        }
+
+        public LocalDateTime getDateTime() {
+                return dateTime;
+        }
+
+        public User getAuthor() {
+                return author;
+        }
+
+        public void setAuthor(User author) {
+                this.author = author;
+        }
+}
